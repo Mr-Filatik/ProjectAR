2b7f40dbaf450432040cf3f59041988e3141ad3e
Mr-Filatik <filatov23wlad@gmail.com>
2021-11-17 16:55:09 +0400

    first version transfusion of color

--- ProjectAR/ProjectAR/Assets/Scripts/ClickScript.cs
+++ ProjectAR/ProjectAR/Assets/Scripts/ClickScript.cs
@@ -6,6 +6,44 @@ public class ClickScript : MonoBehaviour
 {
     [SerializeField] private VostokController vostokController = null;
 
+    private bool isWork = false;
+    private float currentTime = 0f;
+    private bool vector = true;
+    private float cooldownTime = 1f;
+    private Color color = new Color(135 / 256, 126 / 256, 68 / 256, 1);
+
+    private void Update()
+    {
+        if (isWork)
+        {
+            if (currentTime >= cooldownTime)
+            {
+                currentTime = 0f;
+                if (vector)
+                {
+                    vector = false;
+                }
+                else
+                {
+                    vector = true;
+                }
+            }
+            else
+            {
+                if (vector)
+                {
+                    currentTime += Time.deltaTime;
+                    gameObject.GetComponent<MeshRenderer>().material.color = Color.Lerp(Color.black, color, currentTime);
+                }
+                else
+                {
+                    currentTime -= Time.deltaTime;
+                    gameObject.GetComponent<MeshRenderer>().material.color = Color.Lerp(color, Color.black, currentTime);
+                }
+            }
+        }
+    }
+
     private void OnMouseDown()
     {
         vostokController.ClickElement(this); //!!! передача скрипта
@@ -15,11 +53,13 @@ public class ClickScript : MonoBehaviour
     {
         if (state)
         {
+            isWork = true;
             gameObject.GetComponent<MeshRenderer>().material.color = Color.green;
         }
         else
         {
-            gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
+            isWork = false;
+            gameObject.GetComponent<MeshRenderer>().material.color = Color.black;
         }
     }
 }
